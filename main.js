d2fb62cc5f55d7eba45983d90c9474785f5dbcf2
hossain24 <haj.ctg@gmail.com>
2019-01-18 00:33:46 +0100

    changes in PORT

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Data = require('./data');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
 const db = mongoose.connect('mongodb://hossain:hossain24@ds139614.mlab.com:39614/todo', () => {
     console.log('Database is connected');
